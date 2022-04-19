e6757a8211bd63c7d45cfeacc65bf2d336ef95d2
Troy Wever <65945414+WeverTR@users.noreply.github.com>
2022-04-19 13:49:05 -0400

    Created getcustomers get request endpoint

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,78 @@
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestAPI.Models;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly weverMysqlContext _context;
+
+        public CustomerController(weverMysqlContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Customer/getcustomers
+        [HttpGet("getcustomers")]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            var customers = _context.Customers.Where(l => l.User.Email != null).ToList();
+            return customers;
+        }
+
+        // // GET: api/Customer/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(long id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+
+        // POST: api/Customer
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // [HttpPost]
+        // public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+        // {
+        //     _context.Customers.Add(customer);
+        //     await _context.SaveChangesAsync();
+
+        //     return CreatedAtAction("GetCustomer", new { id = customer.Id }, customer);
+        // }
+
+        // // DELETE: api/Customer/5
+        // [HttpDelete("{id}")]
+        // public async Task<IActionResult> DeleteCustomer(long id)
+        // {
+        //     var customer = await _context.Customers.FindAsync(id);
+        //     if (customer == null)
+        //     {
+        //         return NotFound();
+        //     }
+
+        //     _context.Customers.Remove(customer);
+        //     await _context.SaveChangesAsync();
+
+        //     return NoContent();
+        // }
+
+        private bool CustomerExists(long id)
+        {
+            return _context.Customers.Any(e => e.Id == id);
+        }
+    }
+}
